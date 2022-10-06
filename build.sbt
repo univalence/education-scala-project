66f97d2e375120953a6ef65d50aec9b5718c5d60
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-10-06 23:40:20 +0200

    un début de serveur + un rename

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ val scala3Version = "3.2.0"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "My beautiful project", // TODO: name your project
+    name := "A simple webservice in SCALA", // TODO: name your project
     version := "0.1.0-SNAPSHOT",
     developers := List( // TODO: replace the following developer by your team developers
       Developer(
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import java.io.InputStream
+import java.net.ServerSocket
+import java.net.Socket
 
-def msg = "I was compiled by Scala 3. :)"
+
+object Server {
+  def main(args: Array[String]): Unit = {
+    val server = new ServerSocket(80)
+    System.out.println("Démarrage du serveur sur 127.0.0.1:80.\r\nAttente d’une connexion...")
+    val client = server.accept
+    System.out.println("Un client s’est connecté.")
+    val in: InputStream = client.getInputStream
+    val out: OutputStream = client.getOutputStream
+
+    System.out.println("Démarrage du serveur sur 127.0.0.1:80.\r\nAttente d’une connexion...")
+  }
+}
+
+class Server {}
+
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.Socket
+
+
