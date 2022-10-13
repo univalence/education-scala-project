4520ec7bce33de7ad4e2e858e6e6cc3e98ac7b47
Théo Van Rooij <theovanrooij@live.fr>
2022-10-13 09:47:19 +0200

    Add inputs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+// Input of a parser
+// * data: represents the input string
+// * offset: is the pointer on the string, that the parser increases during its processing
+case class Input(data: String, offset: Int = 0):
+  def current(n: Int): String = data.substring(offset,offset+n)
+  def next(n: Int): Input = copy(offset = offset+n)
+  def remaining: String = data.substring(offset)
 
-def msg = "I was compiled by Scala 3. :)"
+// parser.string("A") tild parser.string("BCD")
+// Input("ABCD", 0)
+// first parser => Input("AABCD", 1)
+// deuxième parser => input.current("BC".length)
+
+@main
+def _01_main(): Unit = println(Input("123456789",2).remaining)
