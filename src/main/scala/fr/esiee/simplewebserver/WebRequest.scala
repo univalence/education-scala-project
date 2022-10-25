d20bab6d70262996a2e4310d8fcbe8ab7f7cf293
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-10-25 23:20:46 +0200

    changement des données du WebRequest + ajout de WebParameter

--- src/main/scala/fr/esiee/simplewebserver/WebRequest.scala
+++ src/main/scala/fr/esiee/simplewebserver/WebRequest.scala
@@ -1,11 +1,13 @@
 package fr.esiee.simplewebserver
+import fr.esiee.simplewebserver.WebParameter
 
 enum Method :
   case GET, POST, PUT, DELETE
 
-class WebRequest(r_method : Method, r_path : String, r_headers : List[String], r_content : List[String] = List[String]()) {
-  val path : String = r_path
+class WebRequest(r_method : Method, r_path : String, r_parameters: Seq[WebParameter], r_headers : List[String], r_content : Option[String]) {
   val method : Method = r_method
+  val path : String = r_path
+  val parameters : Seq[WebParameter] = r_parameters
   val headers : List[String] = r_headers
-  val content : List[String] = r_content
+  val content : Option[String] = r_content
 }
